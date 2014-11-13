f5e71073763a3f2c7f979112eb046993de2f9641
Kevin <vm6ej04@gmail.com>
2014-11-13 22:48:25 +1300

    2nd commit

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@ $(document).ready(function(){
 
 	var obj = document.getElementById('object');
 	var debug = document.getElementById('debug');
+	var colorCtrl = document.getElementById('colorCtrl');
+	var speedCtrl = document.getElementById('speedCtrl');
+	var startBtn = document.getElementById('startBtn');
 	var posInfo= $('.co-ord');
 	var touchedInfo= $('.co-ord-last-touched');
 	var offsetInfo=$('.co-ord-offset');
 	var currentX, currentY, touchX, touchY, changeX, changeY;
 	var rResult=0, gResult=200, bResult=255;
-	var sumInfo=$('.digit');
+	var $blocker= $('#blocker');
+	var $distBL= $('.dist-bl');
+	var $angBL= $('.ang-bl');
+	var $sumInfo= $('.digit');
 	var currentR=0,currentG=200,currentB=255;
-	var bg= document.body;
+	var bg= document.getElementById('colorCtrl');
+	var $bg = $('#colorCtrl');
+	var overlay = document.getElementById('overlay');
  	var touchHold;
+ 	var flashing=false;
+ 	var maxY= window.innerHeight;
+ 	var flashingSpeed;
+ 	var defaultSpeed = 800;
 
+
+ 	
+ 
  	function touchAndHold(){
 		touchHold = setTimeout(function(){alert('This is a touch and hold!');},500);
 	}
@@ -31,10 +46,87 @@ $(document).ready(function(){
 		changeX=(event.pageX-currentX);
 		changeY=(event.pageY-currentY);
 	}
+	function flashingFunc(){
+		$bg.velocity("stop",true).velocity({backgroundColor:"#000000"},{duration:100}).velocity("reverse");
+	}
 
+	// if(!window.navigator.standalone) {
+ //    	alert('you can add this WebAPP to your homepage like an usual APP');
+	// }
+//=================== START BUTTON ===================
+	startBtn.addEventListener('touchstart',function(event){
+		 event.preventDefault();
 
-	document.body.addEventListener('touchstart', function(event) { 
+		// if(flashing===false){
+		// 	flashingFunc();
+		// 	flashing=true;
+		// }else{
+		// 	flashing=false;
+		// 	flashingFunc();
+		// }
+
+	},false);
+
+	startBtn.addEventListener('touchend',function(event){
+				 event.preventDefault();
+		 
+		if(flashing===false){
+			// $blocker.velocity("stop").velocity({opacity: 1},{duration: 100});
+			// $blocker.velocity("stop").velocity({opacity:1},{display: "block"});
+			$bg.velocity("stop",true).velocity({opacity: 0, delay: 500},{duration: flashingSpeed},"easeOutExpo").velocity("reverse",{loop: true});
+			flashing=true;
+		}else{
+			// $blocker.velocity("stop").velocity({opacity: 0},{duration: 100});
+			$bg.velocity("stop",true).velocity({opacity: 1},{duration: flashingSpeed});
+			flashing=false;
+		}
+
+	},false);
+//=================== SPEED CONTROL ==================
+	speedCtrl.addEventListener('touchstart', function(event) { 
 		event.preventDefault();
+	  if (event.targetTouches.length == 1) {	  	
+	    var touch = event.targetTouches[0];
+	    touchX= touch.pageX;
+	  	touchY=touch.pageY;
+	  	currentX=touchX;
+	  	currentY=touchY;
+	
+	    posInfo.html("You are now on ("+event.pageX+","+event.pageY+")");
+	  	touchedInfo.html("Last touched co-ordinate ("+currentX+","+currentY+")");
+	  	offsetInfo.html("X moved "+0+", Y moved "+0);
+	  	touchAndHold();	
+	  }
+	}, false);
+
+	speedCtrl.addEventListener('touchmove',function(event){
+		event.preventDefault();
+		if(event.targetTouches.length == 1){
+			var angle = Math.round(Math.atan2(touchX,maxY-touchY)*180/Math.PI);
+			var distance = Math.round(Math.sqrt( Math.pow(touchX,2)+Math.pow(maxY-touchY,2)));
+			var touch = event.targetTouches[0];
+			flashingSpeed= Math.min((angle/90)*defaultSpeed,defaultSpeed);
+			touchX= touch.pageX;
+	  		touchY=touch.pageY;
+	  		$distBL.html("Distance from bottom left: "+distance);
+	  		$angBL.html("Angle: "+angle);
+	  		// $('#angle-pointer').css({"-webkit-transform":"translateX(-50%) rotate("+angle+"deg)"});
+	  		if(distance>150){
+	  			$('#angle-pointer').velocity("stop",true).velocity({ translateX: "-50%", rotateZ: angle+"deg"},{duration: 50});
+		}	}
+		clearTimeout(touchHold);
+	},false);
+
+	speedCtrl.addEventListener('touchend', function(event) { 
+		event.preventDefault();
+	  if (event.targetTouches.length == 1) {	  	
+	    clearTimeout(touchHold);
+	  }
+	}, false);	
+//=================== COLOR CONTROL ==================
+	overlay.addEventListener('touchstart', function(event) { 
+		event.preventDefault();
+		$('#overlay').velocity({opacity:0},{duration: 1000});
 	  if (event.targetTouches.length == 1) {	  	
 	    var touch = event.targetTouches[0];
 	    touchX= touch.pageX;
@@ -52,9 +144,9 @@ $(document).ready(function(){
 	
 	
 
-	document.body.addEventListener('touchmove', function(event) {
+	overlay.addEventListener('touchmove', function(event) {
 		clearTimeout(touchHold);
-			
+		
 			if(rResult===0 && gResult===0 && bResult===255){//// 1
 				if(changeX>=0){
 					rResult=1;
@@ -143,7 +235,7 @@ $(document).ready(function(){
 	  bg.style.backgroundColor="rgb("+rResult+","+gResult+","+bResult+")";
 
 	 
-	  sumInfo.html("RGB("+rResult+","+gResult+","+bResult+")");
+	  $sumInfo.html("RGB("+rResult+","+gResult+","+bResult+")");
 
 	  if (event.targetTouches.length == 1) {
 	  	
